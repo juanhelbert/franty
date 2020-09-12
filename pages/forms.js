9e409218f1cbc3cf5f7ec23959022c592f2ebf4a
Colo <colo@MacBook-Pro-de-Colo.local>
2020-09-12 17:02:29 -0300

    Adding contact form

---
+++ pages/forms.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ParallaxProvider, ParallaxBanner, Parallax } from 'react-scroll-parallax'
+import Head from 'next/head'
+import './index.scss'
+
+export default function Forms() {
+
+  return (
+    <ParallaxProvider>
+      <div className="container">
+        <Head>
+          <title>Forms</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className="content js-content">
+          <p className="copy copy-white">
+            Forms page
+          </p>
+        </main>
+      </div>
+    </ParallaxProvider >
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,158 +1,176 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
+import { ParallaxProvider, ParallaxBanner, Parallax } from 'react-scroll-parallax'
 import Head from 'next/head'
 import './index.scss'
 
+const NUMERO_DE_TELEFONO = '5493364274904'
+
 export default function Home() {
+  const [userInfo, setUserInfo] = useState({})
 
-  useEffect(() => {
-    const body = document.body;
-    const content = document.querySelector('.js-content');
-    const blocks = document.querySelectorAll('.block');
+  const handleChange = e => {
+    const name = e.target.name
+    const value = e.target.value
+    setUserInfo({ ...userInfo, [name]: value })
+  }
 
-    const updateOffset = () => {
-      requestAnimationFrame(updateOffset);
-      body.style.setProperty('--y', content.scrollTop);
-      updateProps();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-    const updateProps = () => {
-      let i = -1;
-      for (let block of blocks) {
-        i += 1;
-        let top = blocks[i].getBoundingClientRect().top;
-        if (top < window.innerHeight * 1.3 && top > window.innerHeight * -1.3) {
-          body.style.setProperty(`--yBlock-${i + 1}`, top);
-        } else {
-          body.style.setProperty(`--yBlock-${i + 1}`, 0);
-        }
-      }
-    }
-
-    updateProps();
-    updateOffset();
-  }, [])
-
+    const formatedText = `
+    *Nombre*: ${(userInfo.name || '').replace(' ', '%20')}
+    %0A*Email*: ${(userInfo.email || '').replace(' ', '%20')}
+    %0A*Mensaje*: ${(userInfo.content || '').replace(' ', '%20')}
+    `
+    window.open(`https://wa.me/${NUMERO_DE_TELEFONO}?text=${formatedText}`)
+  }
 
   return (
-    <div className="container">
-      <Head>
-        <title>Franty</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ParallaxProvider>
+      <div className='container'>
+        <Head>
+          <title>Franty</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
 
-      <main class="content js-content">
-        <section class="block section-landing">
-          <figure class="item-parallax-media">
-            <img src="/images/home-intro.jpg" alt="Imagen intro" />
-          </figure>
-          <div class="item-parallax-content flex-container">
-            <div class="landing-content centered-content">
-              <h1 class="head-large">Franty</h1>
-            </div>
-          </div>
-        </section>
+        <main className='content js-content'>
+          <section class='block section-landing' style={{ paddingTop: 0 }}>
+            <ParallaxBanner
+              style={{ height: '100vh' }}
+              layers={[{
+                image: '/images/home-intro.jpg',
+                amount: 0.5
+              }]}
+            >
+              <h1 className='head-large parallax-text'>Franty</h1>
+            </ParallaxBanner>
+          </section>
 
-        <section class="block section-intro">
-          <div class="item-parallax-content flex-container">
-            <div class="centered-content">
-              <h2 class="head-small head-centered">Nuestra historia</h2>
-              <p class="copy copy-white">
-                Nuestra formación profesional como arquitectos, ha marcado una línea de acción que resuelve la dualidad diseño obra manteniendo el equilibrio indispensable entre ellos.
-              </p>
-              <p class="copy copy-white">
-                La experiencia adquirida durante años de intensa tarea proyectual y constructiva, permite generar proyectos contundentes y simples, que dan como resultado obras económicas y de plazos acotados.
-              </p>
-              <p class="copy copy-white">
-                Convencidos de que el Diseño es una cualidad relevante para nuestros clientes, Fundar dedica especial atención a la elaboración del Proyecto en todos sus detalles contando para ello con equipos de profesionales y recursos tecnológicos en permanente capacitación y actualización. La demanda plantea hoy diversas temáticas y Fundar las satisface con criterios de excelencia.
-              </p>
-            </div>
-          </div>
-        </section>
+          <section className='block section-intro'>
+            <div className='flex-container'>
+              <div className='centered-content'>
+                <Parallax y={[30, -10]} tagOuter='figure'>
+                  <h2 className='head-small head-centered'>Nuestra historia</h2>
 
-        <section class="block section-part">
-          <figure class="item-parallax-media">
-            <img src="/images/nuestros-proyectos.jpg" alt="Nuestros proyectos" />
-          </figure>
-          <div class="item-parallax-content flex-container">
-            <div class="centered-content">
-              <h1 class="head-large head-centered">Nuestros <br />proyectos</h1>
-              {/* <p class="copy copy-white">
-                One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.
-              </p> */}
-            </div>
-          </div>
-        </section>
-
-        <section class="block proyects-grid">
-          <div class="item-parallax-content flex-container img-grid">
-            <figure class="img-gridItem type-right">
-              <img src="/images/proyecto-1.jpg" alt="Free Soul" />
-              <figcaption class="img-caption">
-                <h2 class="head-small">Free Soul</h2>
-                <p class="copy copy-white">
-                  Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+                  <p className='copy copy-white'>
+                    Nuestra formación profesional como arquitectos, ha marcado una línea de acción que resuelve la dualidad diseño obra manteniendo el equilibrio indispensable entre ellos.
+                  </p>
+                  <p className='copy copy-white'>
+                    La experiencia adquirida durante años de intensa tarea proyectual y constructiva, permite generar proyectos contundentes y simples, que dan como resultado obras económicas y de plazos acotados.
+                  </p>
+                  <p className='copy copy-white'>
+                    Convencidos de que el Diseño es una cualidad relevante para nuestros clientes, Fundar dedica especial atención a la elaboración del Proyecto en todos sus detalles contando para ello con equipos de profesionales y recursos tecnológicos en permanente capacitación y actualización. La demanda plantea hoy diversas temáticas y Fundar las satisface con criterios de excelencia.
                 </p>
-              </figcaption>
-            </figure>
-            <figure class="img-gridItem type-left">
-              <img src="/images/proyecto-2.jpg" alt="Free Mind" />
-              <figcaption class="img-caption">
-                <h2 class="head-small">Free Mind</h2>
-                <p class="copy copy-white">
-                  A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets.
-                </p>
-              </figcaption>
-            </figure>
-            <figure class="img-gridItem type-right">
-              <img src="/images/proyecto-3.jpg" alt="Free Air" />
-              <figcaption class="img-caption">
-                <h2 class="head-small">Free Air</h2>
-                <p class="copy copy-white">
-                  Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
-                </p>
-              </figcaption>
-            </figure>
-          </div>
-        </section>
-
-        <section class="block section-end">
-          <figure class="item-parallax-media">
-            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/46992/noah-silliman-141979.jpg" alt="" />
-          </figure>
-          <div class="item-parallax-content flex-container">
-            <div class="landing-content centered-content">
-              <h1 class="head-large">Back to Basic</h1>
+                </Parallax>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section class="custom-properties-ftw">
-          <h3 class="head-small head-centered">Aca van las redes sociales</h3>
-        </section>
-      </main>
+          <ParallaxBanner
+            layers={[{
+              image: '/images/nuestros-proyectos.jpg',
+              amount: 0.5,
+            }]}
+            style={{ height: '100vh' }}
+          >
+            <h2 className='head-large parallax-text'>Nuestros <br />proyectos</h2>
+          </ParallaxBanner>
 
-      <footer>
-      </footer>
+          <section className='block proyects-grid'>
+            <div className='item-parallax-content flex-container img-grid'>
+              <Parallax x={[-15, 5]} y={[40, 0]} tagOuter='figure'>
+                <figure className='img-gridItem type-right'>
+                  <img src='/images/proyecto-1.jpg' alt='Free Soul' />
+                  <figcaption className='img-caption'>
+                    <h2 className='head-small'>Free Soul</h2>
+                    <p className='copy copy-white'>
+                      Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+                  </p>
+                  </figcaption>
+                </figure>
+              </Parallax>
 
-      <style jsx>{`
-        
+              <Parallax x={[15, -5]} y={[40, 0]} tagOuter='figure'>
+                <figure className='img-gridItem type-left'>
+                  <img src='/images/proyecto-2.jpg' alt='Free Mind' />
+                  <figcaption className='img-caption'>
+                    <h2 className='head-small'>Free Mind</h2>
+                    <p className='copy copy-white'>
+                      A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets.
+                </p>
+                  </figcaption>
+                </figure>
+              </Parallax>
 
-      `}</style>
+              <Parallax x={[-15, 5]} y={[40, 0]} tagOuter='figure'>
+                <figure className='img-gridItem type-right'>
+                  <img src='/images/proyecto-3.jpg' alt="Free Air" />
+                  <figcaption className="img-caption">
+                    <h2 className="head-small">Free Air</h2>
+                    <p className="copy copy-white">
+                      Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+                </p>
+                  </figcaption>
+                </figure>
+              </Parallax>
+            </div>
+          </section>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+          <ParallaxBanner
+            layers={[{
+              image: '/images/contacto.jpg',
+              amount: 0.5,
+            }]}
+            style={{ height: '35vh', marginTop: '50vh' }}
+          >
+            <h3 className="head-large parallax-text">Contacto</h3>
+          </ParallaxBanner>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+          <section className='contact-form'>
+            <div className='flex-container'>
+              <div className='centered-content'>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    required
+                    name='name'
+                    type='text'
+                    placeholder='Nombre'
+                    value={userInfo.name}
+                    onChange={e => handleChange(e)}
+                  />
+
+                  <input
+                    required
+                    name='email'
+                    type='email'
+                    placeholder='Email'
+                    value={userInfo.email}
+                    onChange={e => handleChange(e)}
+                  />
+
+                  <textarea
+                    required
+                    type='text'
+                    name='content'
+                    placeholder='Mensaje'
+                    onChange={e => handleChange(e)}
+                  />
+
+                  <button type='submit'>Enviar</button>
+                </form>
+              </div>
+            </div>
+          </section>
+
+          <section className='custom-properties-ftw'>
+            <h3 className='head-small head-centered'>Contacto</h3>
+          </section>
+        </main>
+
+        <footer>
+        </footer>
+
+      </div>
+    </ParallaxProvider >
   )
 }
