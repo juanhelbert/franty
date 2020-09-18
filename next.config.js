4871853144f782fb11ae281c8e6b67fa35a44ad3
Colo <colo@MacBook-Pro-de-Colo.local>
2020-09-18 18:24:04 -0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({})
+module.exports = withSass({
+  env: {
+    'NEXT_PUBLIC_CONTENTFUL_SPACE_ID': process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    'NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN': process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+  }
+})
