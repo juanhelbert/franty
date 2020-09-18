201e89ea0ef0b41f18a462edef6a0e70738a052e
Colo <colo@MacBook-Pro-de-Colo.local>
2020-09-18 18:47:07 -0300

    adding vercel.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  env: {
-    'NEXT_PUBLIC_CONTENTFUL_SPACE_ID': process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    'NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN': process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-  }
-})
+module.exports = withSass({})
