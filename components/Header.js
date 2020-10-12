605fe04fbed25fc11c1fe8b70546aebe06accec7
micaela <mb.mastrovincenzo@gmail.com>
2020-10-12 00:20:43 -0300

    new header mobile

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,37 @@
-import React from 'react'
 import Link from 'next/link'
+import React, { useState } from 'react'
 
 export const Header = () => {
+  const [active, setActive] = useState(false)
   return (
     <header>
       <div className='head-logo'>
         <Link href="/"><img src="/images/Logo.png" alt="The logo"></img></Link>
       </div>
-      <nav className='head-links'>
+      <nav className='head-links-desktop'>
         <ul>
           <li><a href="/#projects">Nuestros Proyectos</a></li>
           <li><Link href="/forms"><a>Tu lugar</a></Link></li>
           <li className="contact"><a href="/#contact">Contacto</a></li>
         </ul>
       </nav>
+
+      <div class="burger-menu" onClick={() => setActive(!active)}>
+            <i class="fa fa-bars"></i>
+       </div>
+
+       
+
+        
+      <div>
+        <nav >
+          <ul className={active ? 'nav-test active' : 'nav-test'}>
+              <li><a href="/#projects">Nuestros Proyectos</a></li>
+              <li><Link href="/forms"><a>Tu lugar</a></Link></li>
+              <li className="contact"><a href="/#contact">Contacto</a></li>
+            </ul>
+        </nav>
+      </div>
 
     </header>
   )
--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
       <Head>
         <title>Franty</title>
         <link rel='icon' href='/favicon.ico' />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
 
       <div className='container'>
